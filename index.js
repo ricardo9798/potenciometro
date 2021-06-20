fb23bd0bc79eca97af641178b6178a2bb4f7d307
ricardo9798 <85805161+ricardo9798@users.noreply.github.com>
2021-06-19 20:53:24 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -7,11 +7,21 @@ const config = {
 
 firebase.initializeApp(config);
 
-const preObject=document.getElementById('/potenciometro-c8c82-default-rtdb/-Mc1pva_mcNEKMdJn7nr');
+const preObject=document.getElementById('/potenciometro-c8c82-default-rtdb/-McX-cUorfzvL5abGQk_');
+const preObject1=document.getElementById('/potenciometro-c8c82-default-rtdb/-McawCGboopt4arIqaDk');
+const preObject2=document.getElementById('/potenciometro-c8c82-default-rtdb/-Mcay7Haxxqn7BMiSChE');
 
-const dbRefObject=firebase.database().ref().child('/potenciometro-c8c82-default-rtdb/-Mc1pva_mcNEKMdJn7nr');
+const dbRefObject=firebase.database().ref().child('/potenciometro-c8c82-default-rtdb/-McX-cUorfzvL5abGQk_');
+const dbRefObject1=firebase.database().ref().child('/potenciometro-c8c82-default-rtdb/-McawCGboopt4arIqaDk');
+const dbRefObject2=firebase.database().ref().child('/potenciometro-c8c82-default-rtdb/-Mcay7Haxxqn7BMiSChE');
 
 dbRefObject.on('value',snap=>{
     preObject.innerText=JSON.stringify(snap.val(),3)});
+
+dbRefObject1.on('value',snap=>{
+    preObject1.innerText=JSON.stringify(snap.val(),3)});
+      
+dbRefObject2.on('value',snap=>{
+    preObject2.innerText=JSON.stringify(snap.val(),3)});
 
     
